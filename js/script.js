4e1da9be1556252bd2ce6b25800df2131dba37c8
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-10-31 13:14:30 -0300

    regex string for seach

--- js/script.js
+++ js/script.js
@@ -4,17 +4,28 @@ console.log(item);
 
 const input = document.querySelector('input');
 
-input.addEventListener('change', handleChange)
+input.addEventListener('change', handleChange);
+input.addEventListener('keyup', valueNone);
+
+function valueNone() {
+  if(input.value === '') {
+    item.forEach(e => e.classList.remove('r'));
+  }
+}
 
 function handleChange() {
+  item.forEach(e => e.classList.remove('r'));
   item.forEach(e => {
-    e.classList.remove('s');
     const topic = e.querySelector('h1').innerText;
-    if (topic === input.value) {
-      // console.log(e);
+    const value = input.value;
+    if(changeString(topic) === changeString(value)) {
+      // console.log(input.value);
     } else {
       e.classList.add('r');
     }
   })
 }
 
+function changeString(str) {
+  return str.toLowerCase().normalize("NFD").replace(/[^a-zA-Z\s]/g, "");
+}
