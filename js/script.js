bcd925527bc43a129d5cd9b98c02997e13191338
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-11-19 00:44:55 -0300

    acertos

--- js/script.js
+++ js/script.js
@@ -1,44 +1,5 @@
 import { Search } from './searchScript.js';
 
-const searchBar = new Search('input', 'container');
+const searchBar = new Search('input', 'container', 'h1', 'div');
 searchBar.init();
 searchBar.focusOnLoad();
-
-
-// const item = document.querySelectorAll('.item');
-// const input = document.querySelector('input');
-
-// input.addEventListener('change', handleChange);
-// input.addEventListener('keyup', valueNone);
-
-// function valueNone() {
-//   if(input.value === '') {
-//     item.forEach(e => {
-//       e.classList.remove('r');
-//       e.classList.add('anim');
-//       setTimeout(()=> {
-//         e.classList.remove('anim');
-//       },300)
-//     });
-//   }
-// }
-
-// function handleChange() {
-//   item.forEach(e => e.classList.remove('r'));
-//   item.forEach(e => {
-//     const topic = e.querySelector('h1').innerText;
-//     const value = input.value;
-//     if(changeString(topic) === changeString(value)) {
-//       e.classList.add('anim');
-//       setTimeout(()=> {
-//         e.classList.remove('anim');
-//       },300)
-//     } else {
-//       e.classList.add('r');
-//     }
-//   })
-// }
-
-// function changeString(str) {
-//   return str.toLowerCase().normalize("NFD").replace(/[^a-zA-Z\s]/g, "");
-// }
